55b33251f8e9af223e1f8a2ea40322a8d4e4c6c0
Armin Talaie <talaiearmin78@gmail.com>
2023-06-10 18:58:08 -0700

    eslit updates

--- bin/app.ts
+++ bin/app.ts
@@ -6,8 +6,6 @@ import { PostingsStack } from '../lib/postings-stack';
 
 // This is the entry point of your CDK app. The `cdk.json` file tells the CDK Toolkit how to execute the app.
 const app = new cdk.App();
-const usersStack: UserStack = new UserStack(app, 'users-stack', {});
-
-const projectsStack: UserStack = new ProjectsStack(app, 'projects-stack', {});
-
-const postingsStack: UserStack = new PostingsStack(app, 'postings-stack', {});
+new UserStack(app, 'users-stack', {});
+new ProjectsStack(app, 'projects-stack', {});
+new PostingsStack(app, 'postings-stack', {});
