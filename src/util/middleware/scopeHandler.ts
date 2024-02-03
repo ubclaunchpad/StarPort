fb6c40b51d4f13e0b951bf1be0679a7bede293de
Will Howell <wrfhowell@gmail.com>
2024-02-03 12:15:45 -0800

    add scope checking

--- src/util/middleware/scopeHandler.ts
+++ src/util/middleware/scopeHandler.ts
@@ -1,8 +1,8 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
+import { Kysely } from 'kysely';
+import { Database } from '../db';
 import { NotFoundError, UnauthorizedError } from './response';
 import { IHandlerEvent, IMiddleware } from './types';
-import {Kysely} from "kysely";
-import {Database} from "../db";
 
 export const ACCESS_SCOPES = {
     ADMIN_READ: 'read:admin',
@@ -16,35 +16,47 @@ export const ACCESS_SCOPES = {
     UPDATE_OWN_PROFILE: 'update:profile:personal',
     ADMIN_DELETE: 'delete:admin',
     DELETE_ALL_PROFILE: 'delete:profile:all',
-    DELETE_OWN_PROFILE: 'delete:profile:personal'
-}
+    DELETE_OWN_PROFILE: 'delete:profile:personal',
+};
 
-export type AccessScope = typeof ACCESS_SCOPES[keyof typeof ACCESS_SCOPES];
+export type AccessScope = (typeof ACCESS_SCOPES)[keyof typeof ACCESS_SCOPES];
 
 export class ScopeController implements IMiddleware<IHandlerEvent, object> {
     private connection: Kysely<Database>;
 
-    public static verifyScopes(scopes: AccessScope[], requiredScopes: AccessScope[]) {
-        const missingScopes = requiredScopes.filter(scope => !scopes.includes(scope));
+    public static verifyScopes(
+        scopes: AccessScope[],
+        requiredScopes: AccessScope[]
+    ) {
+        const missingScopes = requiredScopes.filter(
+            (scope) => !scopes.includes(scope)
+        );
         console.log(missingScopes);
 
         if (missingScopes.length > 0) {
-            throw new UnauthorizedError(`Missing scopes: ${missingScopes.join(', ')}`);
+            throw new UnauthorizedError(
+                `Missing scopes: ${missingScopes.join(', ')}`
+            );
         }
     }
 
-
     public handler = async (event: APIGatewayProxyEvent) => {
-        const userEmail = (event as unknown as { googleAccount: { email: string } })
-            .googleAccount.email;
-        const user = await this.connection.selectFrom('person').select(['email']).where('email','=', userEmail).executeTakeFirst();
+        const userEmail = (
+            event as unknown as { googleAccount: { email: string } }
+        ).googleAccount.email;
+        const user = await this.connection
+            .selectFrom('person')
+            .select(['email'])
+            .where('email', '=', userEmail)
+            .executeTakeFirst();
         if (!user) {
             throw new NotFoundError('User not found');
         }
 
-        const scopes = await this.connection.selectFrom('scope_role')
-            .innerJoin('role', 'scope_role.role_label', 'role.label')
-            .innerJoin('person_role', 'role.id', 'person_role.role_id')
+        const scopes = await this.connection
+            .selectFrom('scope_role')
+            .innerJoin('role', 'role.label', 'scope_role.role_label')
+            .innerJoin('person', 'person.person_role_id', 'role.id')
             .innerJoin('person', 'person_role.user_id', 'person.id')
             .select('scope_role.scope_label')
             .where('person.email', '=', userEmail)
@@ -53,11 +65,11 @@ export class ScopeController implements IMiddleware<IHandlerEvent, object> {
         console.log(scopes);
 
         return {
-            userScopes: scopes.map(scope => scope.scope_label)
-        }
+            userScopes: scopes.map((scope) => scope.scope_label),
+        };
     };
 
-    constructor( connection: Kysely<Database>) {
+    constructor(connection: Kysely<Database>) {
         this.connection = connection;
     }
 }
