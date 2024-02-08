e016a38e2116fd56dc9c9a8985785eb4f74e8668
Xavier Lam <reivax685@gmail.com>
2024-02-07 23:52:26 -0800

    added paging searching and filtering WIP

--- src/users/getUsers.ts
+++ src/users/getUsers.ts
@@ -6,41 +6,68 @@ import {LambdaBuilder, LambdaInput} from '../util/middleware/middleware';
 import { APIResponse, SuccessResponse } from '../util/middleware/response';
 import { PaginationHelper, ResponseMetaTagger } from '../util/middleware/paginationHelper';
 
-const db = getDatabase();
 
 const DEFAULT_LIMIT = 50;
-const OFFSET = 0;
-export const handler = new LambdaBuilder(getRequest)
+const DEFAULT_OFFSET = 0;
+const db = getDatabase();
+
+export const handler = new LambdaBuilder(getUserRequest)
     .use(new InputValidator())
     // .use(new Authorizer())
-    .use(new PaginationHelper({ limit: DEFAULT_LIMIT, offset: OFFSET}))
+    .use(new PaginationHelper({ limit: DEFAULT_LIMIT, offset: DEFAULT_OFFSET}))
     .useAfter(new ResponseMetaTagger())
     .build();
 
-export async function getRequest(
-    event: LambdaInput
-): Promise<APIResponse> {
-    const personQuery = ((event && event.queryStringParameters) ||
-        {}) as unknown as IPersonQuery;
+export async function getUserRequest(event: LambdaInput): Promise<APIResponse> {
+    if (event.pagination) {
+        const count = await countUsers();
+        event.pagination.count = count;
+    }
+
+    const personQuery = ((event && event.pagination) ||
+        {}) as unknown as IPersonQuery;        
+
+    if (event.queryStringParameters && event.queryStringParameters.search) {
+        personQuery.search = event.queryStringParameters.search;
+    }
+    
+    if (event.queryStringParameters && event.queryStringParameters.filter) {
+        personQuery.filter = JSON.parse(event.queryStringParameters.filter);
+    }
+
     return new SuccessResponse(await getAll(personQuery));
 }
 
 export async function getAll(personQuery: IPersonQuery) {
-    const res = await db
-        .selectFrom('person')
-        .select( [
-            'person.id',
-            'person.first_name',
-            'person.last_name',
-            'person.pref_name',
-            'person.person_role_id',
-            'person.email',
-            'person.account_updated',
-            'person.member_since'
-            ])
-        .limit(personQuery.limit || 10)
-        .offset(personQuery.offset || 0)
-        .execute();
+    let query = db.selectFrom('person').select([
+        'person.id',
+        'person.first_name',
+        'person.last_name',
+        'person.pref_name',
+        'person.person_role_id',
+        'person.email',
+        'person.account_updated',
+        'person.member_since'
+    ]);
+
+    // Apply limit and offset
+    query = query.limit(personQuery.limit || DEFAULT_LIMIT).offset(personQuery.offset || DEFAULT_OFFSET);
+
+    // Apply search if provided
+    if (personQuery.search) {
+        query = query.where(`person.first_name`, 'like', `%${personQuery.search.toLowerCase()}%`);
+        //WIP use full name or more values to search (it works rn with just first name tho!)
+    }
+
+    // Apply filtering by role
+    if (personQuery.filter?.person_role_id) {
+        query = query.where('person.person_role_id', '=', personQuery.filter.person_role_id);
+    }
+
+    // Update to add more filters as needed
+    // Status and Project Team currently don't exist in the database for Person, but can be added
+
+    const res = await query.execute();
 
     return res.map((user) => {
         return {
@@ -54,4 +81,16 @@ export async function getAll(personQuery: IPersonQuery) {
             member_since: user.member_since,
         };
     });
+}
+
+async function countUsers() {
+    const ret = await db.selectFrom('person')
+    .select(({ fn }) => [
+      fn.count<number>('id').as('count'),
+    ])
+    .executeTakeFirst();
+    if (!ret) {
+      throw new Error('Unable to count users');
+    }
+    return Number(ret.count);
 }
--- src/util/types/general.ts
+++ src/util/types/general.ts
@@ -3,6 +3,16 @@ import { Person } from '../db';
 export interface IPagination {
     limit: number;
     offset: number;
+    count?: number;
 }
 
-export type IPersonQuery = Partial<Person> & IPagination;
+export interface IPersonQuery extends Partial<Person>, IPagination{
+    search?: string;
+    filter?: {
+        first_name?: string;
+        person_role_id?: number;
+        status?: string;
+        project_team?: string;
+        // Add more filter options as needed
+    };
+}
