2c68be323a870a3591e8a0a9d8811f33aeac1e0b
Armin Talaie <talaiearmin78@gmail.com>
2024-03-28 22:33:52 -0700

    wiki sql simplification

--- resources/database/migrations/1710395365.sql
+++ resources/database/migrations/1710395365.sql
@@ -1,37 +1,20 @@
 -- Create Area table
-CREATE TABLE IF NOT EXISTS Area (
+CREATE TABLE IF NOT EXISTS area (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     description VARCHAR(255),
-    numberOfDocs INT,
     lastUpdatedDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    parentAreaID INT
+    parent_areaid INT DEFAULT NULL,
+    Foreign Key (parent_areaid) references area(id) ON DELETE NO ACTION
 );
 
 -- Create Documents table
-CREATE TABLE IF NOT EXISTS Documents (
+CREATE TABLE IF NOT EXISTS document (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    areaID INT,
-    title VARCHAR(255),
-    docLink VARCHAR(255),
-    lastEditedUser VARCHAR(255),
+    areaid INT,
+    title VARCHAR(500),
+    doclink VARCHAR(2000),
     creationDate DATE,
     lastUpdatedDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    -- FOREIGN KEY (areaID) REFERENCES Area(id)
-);
-
--- Create Tags table
-CREATE TABLE IF NOT EXISTS Tags (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    tagName VARCHAR(255) UNIQUE
-);
-
--- Create junction table to represent the many-to-many relationship between Documents and Tags
-CREATE TABLE IF NOT EXISTS DocumentTags (
-    docID INT,
-    tagID INT,
-    PRIMARY KEY (docID, tagID)
-    -- FOREIGN KEY (docID) REFERENCES Documents(id),
-    -- FOREIGN KEY (tagID) REFERENCES Tags(id)
+    Foreign Key (areaid) references area(id) ON DELETE CASCADE
 );
