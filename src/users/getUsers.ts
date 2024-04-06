87522b9da1c79e423bb9c19cfa5b7a648cc13839
Xavier Lam <reivax685@gmail.com>
2024-04-06 12:41:49 -0700

    fixed getusers endpoints

--- src/users/getUsers.ts
+++ src/users/getUsers.ts
@@ -60,7 +60,7 @@ export async function getAll(personQuery: IPersonQuery) {
             'person.first_name',
             'person.last_name',
             'person.pref_name',
-            'person.person_role_id',
+            // 'person.person_role_id',
             'person.email',
             'person.account_updated',
             'person.member_since',
